0b58cfba18ad36677ddb71f6b7c086b9647885a6
Nahom <zerihunnahom@gmail.com>
2023-03-30 01:23:01 +0300

    Update BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,11 +8,14 @@ const BookList = () => {
   ];
   return (
     <section>
-      <ul>
-        {books.map((book) => <Book key={book.id} bookInfo={book} />)}
-      </ul>
+      <div className="booklist-wrapper">
+        <h1>Books Available in Bookstore</h1>
+        <ul className="booklist">
+          {books.map((book) => <Book key={book.id} bookInfo={book} />)}
+        </ul>
+      </div>
       <div className="add-book-wrapper">
-        <h3 className="add-book-header">ADD NEW BOOK</h3>
+        <h2 className="add-book-header">ADD NEW BOOK</h2>
         <form className="add-book">
           <input type="text" placeholder="Book title" className="book-title-input" />
           <input type="text" placeholder="Author" className="book-author-input" />
