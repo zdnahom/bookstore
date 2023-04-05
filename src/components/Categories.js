474c857dc5d6dfbc16f8c6e38bca0f45562f38b4
Nahom <zerihunnahom@gmail.com>
2023-04-05 12:30:40 +0300

    Implement categoriesSlice on Categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,15 @@
-const Categories = () => (
-  <section className="categories">
-    <button type="button" className="check-status-button">check status</button>
-  </section>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/features/categories/categoriesSlice';
+
+const Categories = () => {
+  const { categories } = useSelector((store) => store.categories);
+  const dispatch = useDispatch();
+  return (
+    <section className="categories">
+      <h2>{categories}</h2>
+      <button type="button" className="check-status-button" onClick={() => dispatch(checkStatus())}>check status</button>
+    </section>
+  );
+};
 
 export default Categories;
