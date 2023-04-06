494f194bea654a5bcfb9c0479243c8dd3d1f7795
Nahom <zerihunnahom@gmail.com>
2023-04-06 12:45:26 +0300

    Add styling to Navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './styles/App.css';
 import BookList from './components/BookList';
 import Categories from './components/Categories';
 import Navigation from './components/Navigation';
